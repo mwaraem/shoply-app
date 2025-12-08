dba898215c52321ae9d138882cbcd1c46268dd9a
mwaraem <mwaraem@gmail.com>
2025-12-08 15:51:00 +0300

    Fix

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -25,7 +25,7 @@ export const issueToken = (res, user) => {
     res.cookie("token", token, {
         httpOnly: true,
         sameSite: "lax",
-        secure: true, // set to true in production
+        secure: process.env.NODE_ENV === 'production', // set to true in production
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         path: '/'
     });
